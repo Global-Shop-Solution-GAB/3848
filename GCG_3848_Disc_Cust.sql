c49edefbc3a70d2327cc6f6a4b957ddab7bc9a62
Service Web <sw@gssmail.com>
2024-03-08 16:42:55 -0600

    Commit changes in C:\Websites\Git\emarlow\3848\GCG_3848_Disc_Cust.sql

---
+++ GCG_3848_Disc_Cust.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "GCG_3848_Disc_Cust"(
+ "Rec_ID" INTEGER,
+ "Customer" CHAR(6),
+ "Customer_Type" CHAR(30),
+ "Customer_Class" CHAR(30),
+ "Price_Group" CHAR(30),
+ "Price_Category" CHAR(30),
+ "Min_Net_Sales" INTEGER,
+ "Disc_Code" CHAR(30),
+PRIMARY KEY ("Rec_ID"));
